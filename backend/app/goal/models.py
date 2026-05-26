657e3a30e59e45e5345c92837d5956253ac1c0ee
Amon_er <foolu6972@gmail.com>
2026-05-26 22:18:52 +0300

    changed the default value

--- backend/app/goal/models.py
+++ backend/app/goal/models.py
@@ -17,7 +17,7 @@ class Goal(Base):
     title: Mapped[str] = mapped_column(String(20))
     description: Mapped[str | None] = mapped_column(String(255), default=None)
     link: Mapped[str | None] = mapped_column(String(2048), default=None)
-    img_path: Mapped[str | None] = mapped_column(String(255), default='images/placeholder.png')
+    img_path: Mapped[str | None] = mapped_column(String(255), default='./backend/images/placeholder.png')
     goal_amount: Mapped[Decimal] = mapped_column(Numeric(11, 2))
     start_date: Mapped[date | None] = mapped_column(Date)
     end_date: Mapped[date | None] = mapped_column(Date)
